b1934db82f85284d58bf33d1c1b8ab3e1680cb7a
manur <manur@DESKTOP-UFTN14N>
2020-12-17 13:54:40 +0100

    Parte Obligatoria del trabajo acabada

--- Practica1MarcosAD/src/main/java/ProyectoMarcos/App/App.java
+++ Practica1MarcosAD/src/main/java/ProyectoMarcos/App/App.java
@@ -1,56 +1,36 @@
 package ProyectoMarcos.App;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 import ProyectoMarcos.BBDD.BbddMarcos;
 
-/**
- * Hello world!
- *
- */
 public class App {
+	static Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) throws SQLException {
 		Connection c = BbddMarcos.conectar();
-		try {
-			// El objeto utilizado para ejecutar una declaración SQL estática y
-			// devolver los resultados que produce.
-
-			Statement stm = null;
-			ResultSet rs = null;
-			try {
-				stm = c.createStatement();
-			} catch (SQLException e1) {
-				System.out.println("Error al crear el statement");
-				e1.printStackTrace();
+		boolean exit = true;
+		do {
+			Funciones.menu();
+			int op = kb.nextInt();
+			switch (op) {
+			case 1:
+				Funciones.añadirUnCliente(c);
+				break;
+			case 2:
+				Funciones.mostrarUnCliente(c);
+				break;
+			case 3:
+				Funciones.mostrarTodosLosCliente(c);
+				break;
+			case 6:
+				exit = false;
+				break;
+			default:
+				throw new IllegalArgumentException("Valor erroneo: " + op);
 			}
-			if (stm != null) {
-				try {
-					rs = stm.executeQuery("SELECT * FROM jardineria.cliente");
-					while (rs.next()) {
-
-						System.out.println(rs.getString("nombre_contacto"));
-
-					}
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}
-
-			
-			/*
-			 * Statement statement = c.createStatement(); ResultSet resultSet =
-			 * statement.executeQuery("select * from empleado"); while (resultSet.next()) {
-			 * String codigoEmpleado = resultSet.getString("codigo_empleado"); String nombre
-			 * = resultSet.getString("nombre"); } c.close();
-			 */
-			 
-
-		} catch (Exception e2) {
-			e2.printStackTrace();
-		}
-
+		} while (exit);
 	}
 }
